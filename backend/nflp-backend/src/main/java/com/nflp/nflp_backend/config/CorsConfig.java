5527c6ec06cae9b03293a717ab619f4981b827ef
russellchao <russchaoman@gmail.com>
2026-01-07 20:45:52 -0500

    Added Procfile and AWS Amplify link to Backend cors

--- backend/nflp-backend/src/main/java/com/nflp/nflp_backend/config/CorsConfig.java
+++ backend/nflp-backend/src/main/java/com/nflp/nflp_backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")  // Apply to all /api/* endpoints
-                        .allowedOrigins("http://localhost:3000")  // React dev server
+                        .allowedOrigins("http://localhost:3000", "https://master.d1pdg2blrhvj6x.amplifyapp.com/")  // React dev and production server
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
